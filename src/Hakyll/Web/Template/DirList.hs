75099568cfe78179c1084683e1913625bf93948e
Robert Hennig <robert@freylax.de>
2018-02-12 22:10:07 +0100

    bumped to hakyll 4.11

--- src/Hakyll/Web/Template/DirList.hs
+++ src/Hakyll/Web/Template/DirList.hs
@@ -136,14 +136,45 @@ getItemPageId id' = do
   metadata <- getMetadata id'
   return $ fromMaybe
     ( takeBaseName $ toFilePath id' )
-    ( M.lookup "page-id" metadata )
+    ( lookupString "page-id" metadata )
 
 getItemPageOrder :: MonadMetadata m => Identifier -> m String
 getItemPageOrder id' = do
   metadata <- getMetadata id'
   pageId <- getItemPageId id'
-  return $ fromMaybe pageId ( M.lookup "page-order" metadata)
+  return $ fromMaybe pageId ( lookupString "page-order" metadata)
 
+{-|
+  The exported 'dirListField' function is similar to the
+  'Hakyll.Web.Template.listField' template function but creates
+  additional context information which can be used in the template
+  to create a hierarchical menu.
+
+  == Context usable inside the template 
+
+  [@$begin-tags$@]: injects @\<li\>@ and @\<ul\>@ tags if apropriate
+
+  [@$end-tags$@]: contains the corresponding @\<\/li\>@ and @\<\/ul\>@ tags
+
+  [@$full-page-id$@]: is the hyphen seperated path of the page
+
+  == Metainformation in the source files
+  For each subdirectory which should be processed one source file
+  with the same base name should
+  exist which can contain meta information:
+
+  [@pages\/a.md@]:       top page for directory a
+
+  [@pages\/a\/foo.md@]:   page foo within a
+
+  The following meta information can be given
+
+  [@page-id@]:          part of the generated id, if not given the base name of the file
+
+  [@page-order@]:       give an ordering key for sorting in the current
+                       directory level, if not given the @page-id@ will be used
+
+-}
 dirListField :: String -> Context a -> Compiler [Item a] -> Context b
 dirListField key c xs = listField key ( c' `mappend` c) pages' 
   where
